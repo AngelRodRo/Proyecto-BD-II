587c1c790c6593b36d72a2c5c58fc488c28f5357
AngelRodRo <aleangrodriguez321@hotmail.com>
2014-11-26 18:23:08 -0500

    Busqueda de vuelos

---
+++ principal.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<form action="
+		<label for="">Ciudad de origen:</label>
+		<input type="text">
+		<label for="">Ciudad de destino:</label>
+		<input type="text">
+		<label for="">Fecha de partida :</label>
+		<input type="text">
+
+
+	"></form>
+</body>
+</html>
