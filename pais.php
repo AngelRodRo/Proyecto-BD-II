641a1d04e16421c914130e59735c6758d1eb8b34
AngelRodRo <aleangrodriguez321@hotmail.com>
2014-11-29 06:20:20 -0500

    Cambiando diseño de pais

--- pais.php
+++ pais.php
@@ -1,13 +1,49 @@
 <!DOCTYPE html>
 <html lang="es">
-	<head>
-		<title>Paises</title>
-		<meta charset="utf-8">
-	</head>
-	<body>
+	 
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/starter-template.css" rel="stylesheet">
+<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img src="img/logo.png" width="70" height="30" alt=""></a>
+          <a class="navbar-brand" href="#">Agencia de viajes</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="index1.php">Inicio</a></li>
+ 
+            <li><a href="ayuda.php">Ayuda</a></li>
+          </ul>
+           <ul class="nav navbar-nav navbar-right">
+              <li><a href="">Iniciar sesion</a></li>
+              <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-list"><b class="caret"></b></a>
+              <ul class="dropdown-menu">
+              </ul>
+              </li>
+            </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+	<div class="cabecera">
+		<img class="logo" src="img/logo.png" width="250" height="130" alt="">
+ 
+  </div>
+	<div class="separacion"></div>
+	<div class="cuerpo">
 		<h1>Registros de Paises</h1>
-			<form action="scripts/registrar_pais.php" method="POST">
-				<table >
+			<form class="formulario1" action="scripts/registrar_pais.php" method="POST">
+				<table class="pais">
 					<tr>
 						<td>
 							<label for="cod_pais">Código de Pais:</label>
@@ -66,5 +102,7 @@
 ?>
 			<?php echo $tabla; ?>
 			</table>
+			</div>
+			<footer>Derechos reservados</footer>
 	</body>
 </html>
