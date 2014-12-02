1ab5904cc1f8b80d258ce39c2fb0e9069405293e
AngelRodRo <aleangrodriguez321@hotmail.com>
2014-12-02 17:15:36 -0500

    Change

--- SQL/Vistas/registro_pasajero.sql
+++ SQL/Vistas/registro_pasajero.sql
@@ -4,3 +4,4 @@ AS
 SELECT cod_pasajero as 'Codigo',nombre_pasajero as 'Nombre de pasajero',apellido_paterno as 'Apellido Paterno',
 apellido_materno as 'Apellido Materno', Edad = datediff(YEAR,fecha_nacimiento,getdate()),
 email_pasajero as 'Email', sexo as 'Sexo' FROM pasajero	
+	
